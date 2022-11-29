3ea064474174208e6a62b11b9290f110c4bcbbc2
Sergi Alesanco Perich <sergi.alesanco.perich@gmail.com>
2022-11-29 12:47:52 +0100

    add

---
+++ Exercici de fitxers 1000000/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+
+namespace Exercici_de_fitxers_1000000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i=0;
+            int loteria;
+            int cont = 0;
+            string linea = "";
+            Random nums = new Random();
+
+            StreamWriter fitxerW = new StreamWriter(@"C:\Users\sergi\source\repos\Problemes sobre numeros\Exercici de fitxers 1000000\resultats.txt");
+
+
+            while (i < 1000000) {
+
+                
+
+                while (cont < 5)
+                {
+                    loteria = nums.Next(6, 49);
+
+
+                    linea += loteria + " "; 
+                    cont++;
+                    
+
+                }
+                cont = 0;
+                fitxerW.WriteLine($"{linea} ");
+                linea = "";
+
+                i++;
+            }
+
+            fitxerW.Close();
+           
+
+            
+
+
+
+
+
+        }
+    }
+}
